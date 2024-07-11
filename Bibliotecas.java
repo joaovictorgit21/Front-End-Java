c3ab636bb347bb774365136dd6ebd81b654ca1f2
João Victor Farias <111014716+joaovictorgit21@users.noreply.github.com>
2024-07-11 15:49:48 -0300

    Add files via upload

---
+++ Bibliotecas.java
@@ -0,0 +1,8 @@
+import java.awt.*;
+import javax.swing.*;
+
+public class Bibliotecas extends JPanel {
+    public static void main(String[] args) throws Exception {
+        System.out.println(Color.WHITE);
+    }
+}
---
+++ Variaveis.java
@@ -0,0 +1,24 @@
+public class Variaveis { // Declaração da Classe 
+    public static void main(String[] args) throws Exception {
+        int varInt = 2; // Declaração da variavel do tipo "int" que recebe seu nome de "varInt" e seu valor como "2" // Recebe valores inteiros
+        System.out.println(varInt);
+
+        Integer varInteger = 3; // Declaração da variavel do tipo "Integer" que recebe seu nome de "varInteger" e seu valor como "3" // Recebe valores Inteiros limitados
+        System.out.println(varInteger);
+
+        String varString = "texto"; // Declaração da variavel do tipo "String" que recebe seu nome de "varString" e seu valor como "texto" // Recebe valores de Strings como, textos, numeors e caracteres especiais, sempre dentro de aspas duplas ""; 
+        System.out.println(varString);
+
+        boolean varBoolean = true; // Declaração da variavel do tipo "boolean" que recebe seu nome de "varBoolean" e seu valor como "true" or "false"
+        System.out.println(varBoolean);
+
+        byte varByte = 1; // Declaração da variavel do tipo "byte" que recebe seu nome de "varByte" e seu valor como "1"; // 
+        System.out.println(varByte);
+
+        char varChar = 'r'; // Declaração da variavel do tipo "char" que recebe seu nome de "varChan" e seu valor como "r"
+        System.out.println(varChar);
+
+        float varFloat = 19.99f; // Declaração da variavel do tipo "float" que recebe seu nome de "varFloat" e seu valor como "19.99f"
+        System.out.println(varFloat);
+    }
+}
